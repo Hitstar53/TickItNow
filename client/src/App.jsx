6cdc455d7c988aadf5d44a3c775ad2822908f05a
Hatim Sawai <hatimsawai76@gmail.com>
2023-09-25 16:40:31 +0530

    starter #2
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div className='flex justify-center items-center h-screen bg-gray-200'>
-      App
+      App, this has changed!
     </div>
   )
 }
