218d54f42dec87f7bf5663113a663dfcc39adcf0
Mufaddal-Shiyaji <105577870+Mufaddal-Shiyaji@users.noreply.github.com>
2023-10-20 10:21:40 +0530

    Testing getEvents functions -(importing asyncHandler left)

--- server/Controllers/eventsController.js
+++ server/Controllers/eventsController.js
@@ -1,24 +1,25 @@
 import Events from "../Models/eventsModel.js";
+import asyncHandler from "express-async-handler";
 
-const getEvents = async (req, res) => {
+const getEvents = asyncHandler(async (req, res) => {
     try {
-        const events = Events.find({});
+        const events = await Events.find();
         console.log(events);
         res.status(200).json(events);
     } catch (error) {
-        res.status(404).json({ message: error.message });
+        res.status(400).json({ message: error.message });
     }
-}
+})
 
-const getEvent = async (req, res) => {
+const getEvent = asyncHandler(async (req, res) => {
     const { id } = req.params;
     try {
-        const event = Events.findById(id);
+        const event = await Events.findById(id);
         res.status(200).json(event);
     } catch (error) {
-        res.status(404).json({ message: error.message });
+        res.status(400).json({ message: error.message });
     }
-}
+})
 
 export { getEvents, getEvent };
 
