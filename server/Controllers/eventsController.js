a237cb7430e409a29e1b8c376cd93629a114682f
Mufaddal-Shiyaji <mufaddal1693@gmail.com>
2023-10-24 11:29:22 +0530

    set,update,delete event created and tested

--- server/Controllers/eventsController.js
+++ server/Controllers/eventsController.js
@@ -20,5 +20,42 @@ const getEvent = asyncHandler(async (req, res) => {
     }
 })
 
-export { getEvents, getEvent };
+const setEvent = asyncHandler(async(req,res) =>{
+    const title = req.body.title;
+    const description = req.body.description;
+    const image = req.body.image;
+    const price = req.body.price;
+    const rating = req.body.rating;
+    const tags = req.body.tags;
+    try {
+        const event = new Events({title:title,description:description,image:image,price:price,rating:rating,tags:tags})
+        await event.save()
+        res.status(200).json('Event Added Succesfully')
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+const deleteEvent = asyncHandler(async(req,res)=>{
+    try {
+        const id = req.params.id;
+        await Events.findOneAndDelete({'_id':id})
+        res.status(200).json('deleted Succesfully')
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+const updateEvent = asyncHandler(async(req,res)=>{
+    const id = req.params.id
+    const changes = req.body
+    try{
+        await Events.findByIdAndUpdate(id,changes)
+        res.status(200).json('Updated Succesfully')
+    } catch(error){
+        res.status(400).json(error)
+    }
+})
+
+export { getEvents, getEvent, setEvent, deleteEvent, updateEvent };
 
--- server/Routes/eventsRoutes.js
+++ server/Routes/eventsRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import {getEvents,getEvent} from '../Controllers/eventsController.js';
+import {getEvents,getEvent, setEvent, deleteEvent, updateEvent} from '../Controllers/eventsController.js';
 
 const router = express.Router();
 
 router.get('/', getEvents);
 router.get('/:id',getEvent);
-
+router.put('/setEvent',setEvent);
+router.delete('/deleteEvent/:id',deleteEvent);
+router.patch('/updateEvent/:id',updateEvent);
 export default router;
