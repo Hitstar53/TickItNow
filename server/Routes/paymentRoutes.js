76f2581cc21be4b3224594f49dfd292e540fec3d
KaifSayyad <kaifalisayyad@gmail.com>
2023-11-22 18:23:41 +0530

    fixed payment routes error

--- server/Routes/paymentRoutes.js
+++ server/Routes/paymentRoutes.js
@@ -1,18 +1,18 @@
 import express from 'express';
 import{
-    paymentSuccess,
-    paymentFailure,
-    processPayment
+    makePayment,
+    cancelPayment,
+    updateUserDatabase,
+    updateOrganizerDatabase,
+    updateEventDatabase,
 } from '../Controllers/paymentController.js';
 const router = express.Router();
 
-// Route for processing payment
-router.post('/payment', processPayment);
-
-// Route for handling payment success
-router.get('/payment/success', paymentSuccess);
-
-// Route for handling payment failure
-router.get('/payment/failure', paymentFailure);
+router.post('/payment', makePayment);
+router.post('/payment/cancel', cancelPayment);
+router.put('/updateUser', updateUserDatabase);
+router.put('/updateOrganizer', updateOrganizerDatabase);
+router.put('/updateEvent', updateEventDatabase);
 
 export default router;
+
