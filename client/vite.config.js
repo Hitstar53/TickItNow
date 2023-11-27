d4e4eb5a4df87fb9bd4a1a59e27257fbc8ee03dc
Saad-Surve <95091209+Saad-Surve@users.noreply.github.com>
2023-11-27 22:22:52 +0530

    Update Dockerfile and Vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-  },
-})
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 5173, // you can replace this port with any port
+  }}
+  )
